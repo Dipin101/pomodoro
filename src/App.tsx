a82321c74d7881c2c4b6a761c08cf9f1cf64fbb4
Dipin101 <khatridipin10@gmail.com>
2026-03-13 00:39:42 -0400

    finalize pomodoro with clean component structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ const App = () => {
   const { state, start, pause, reset, setMode } = useTimer();
   return (
     <>
-      <div>
-        <h1>Pomodoro</h1>
+      <div className="flex flex-col justify-center items-center gap-7">
+        <h1 className="text-8xl font-bold ">Pomodoro</h1>
         <ModeSelector mode={state.mode} onSetMode={setMode} />
         <Timer
           timeLeft={state.timeLeft}
